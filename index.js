2654257f8c25dc4051442f5ed6c01fb5e2738b9d
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2025-12-08 22:13:53 +0530

    configure stripe package

--- index.js
+++ index.js
@@ -126,6 +126,43 @@ server.use("/auth", authRouter.router);
 server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), orderRouter.router);
 
+// This is your test secret API key.
+const stripe = require("stripe")(
+  "sk_test_51SaJjnLFLV1W6TyFSvVKk9pYBVsvljAN55H6KNj31tntHcEa0Kb5qvcGlu2NVtaMFT3yBGPRWRMNJZbW1H9b2HKU00GlGgsZs4"
+);
+
+server.use(express.static("public"));
+
+const calculateOrderAmount = (items) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  // let total = 0;
+  // items.forEach((item) => {
+  //   total += item.amount;
+  // });
+  // return total;
+  return 1400;
+};
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
 main().catch((err) => console.log("err==>>", err));
 
 async function main() {
